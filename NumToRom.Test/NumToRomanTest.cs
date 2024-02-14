815c5e46d57f16296819e2722a1dbbf4ee7ef997
Gabriel De La Rosa Paniagua <gabriel.delarosa@solvex.com.do>
2024-02-13 23:48:04 -0400

    feat: Test done

--- NumToRom.Test/NumToRomanTest.cs
+++ NumToRom.Test/NumToRomanTest.cs
@@ -5,7 +5,31 @@ namespace NumToRom.Test
         [Fact]
         public void Should_Valid_Numer_2023_To_Return_MMXXIII()
         {
+            Assert.Equal("MMXXIII", Program.NumToRoman(2023));
+        }
 
+        [Fact]
+        public void Should_Valid_Numer_4_To_Return_IV()
+        {
+            Assert.Equal("IV", Program.NumToRoman(4));
+        }
+
+        [Fact]
+        public void Should_Valid_Numer_1994_To_Return_MCMXCIV()
+        {
+            Assert.Equal("MCMXCIV", Program.NumToRoman(1994));
+        }
+
+        [Fact]
+        public void Should_Return_ErrorMessage_If_Number_Is_Out_Of_Range()
+        {
+            Assert.Equal("Número ingresado fuera de rango", Program.NumToRoman(5000));
+        }
+
+        [Fact]
+        public void Should_Valid_Numer_5_To_Return_V()
+        {
+            Assert.Equal("V", Program.NumToRoman(5));
         }
     }
 }
